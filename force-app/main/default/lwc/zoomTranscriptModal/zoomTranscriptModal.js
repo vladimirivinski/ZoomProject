0cf4de71c3f592771a93d4c8545d2288daad9c2e
VladimirIvinsky <vladimir.ivinski89@gmail.com>
2023-01-15 21:38:10 +0300

    refreshed version with lwc modal

--- force-app/main/default/lwc/zoomTranscriptModal/zoomTranscriptModal.js
+++ force-app/main/default/lwc/zoomTranscriptModal/zoomTranscriptModal.js
@@ -1,12 +1,100 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from "lightning/uiRecordApi";
+import { getFields } from "./ZoomTranscriptModalHelper";
+import getCallTranscription from "@salesforce/apex/ZoomCallTranscriptionController.getCallTranscription";
+
+const fields = getFields();
 
 export default class ZoomTranscriptModal extends LightningElement {
-    @api header;
-    @api content;
+    @api recordId;
 
-    // Return a custom value when the modal is closed with the Close button.
-    // If no value is returned in the close method, then undefined is returned(Same as closing with the X button).
-    handleClose() {
-        this.close('return value');
+    error;
+    _isLoading = false;
+    queriedFields = {};
+    queriedData = {};
+    downloadCVS = false;
+    callLog = null;
+    downloadCSVDisable = false;
+    emptyResponse = 'This phone call does not have a conversation record. Please check the value of the "Has Recording" field.'
+
+    @wire(getRecord, { recordId: "$recordId", fields })
+    wiredPhoneCallInfo({ error, data }) {
+        if (data?.fields) {
+            this.queriedFields = data?.fields;
+            this.recordUnavaileble = this.recordingId === null ? true : false;
+        } else {
+            this.error = error;
+        }
     }
+    @wire(getCallTranscription, { recordingId: '$recordingId' })
+    wiredTranscription({ error, data }) {
+        if(data?.timeline){
+            this._isLoading = true;
+            this.queriedData = data?.timeline;
+            this.parseCallLogData();
+        }else {
+            this.error = error;
+        }
+    }
+
+    //================================GETTERS===========================================
+    //==================================================================================
+    get isLoading() {
+        return this._isLoading;
+    }
+    get callerName(){
+        return this.queriedFields?.Name?.value;
+    }
+    get callDate(){
+        return this.queriedFields?.Call_Date_Time__c?.displayValue;
+    }
+    get recordingId(){
+        return this.queriedFields?.recording_Id__c?.value;
+    }
+    get recordingAvaileble(){
+        return this.recordingId === null ? true : false;
+    }
+
+    //=================================CALLBACK=========================================
+    //==================================================================================
+    connectedCallback() {
+    }
+    disconnectedCallback(){
+    }
+    errorCallback(error, stack) {
+        console.error("ZoomTranscriptModal: errorCallback", error, stack);
+    }
+
+
+    //==========================================FUNCTIONS==============================
+    //=================================================================================
+    parseCallLogData(){
+        this.downloadCVS = true;
+        this.callLog = this.queriedData.map(e => {
+            let users = e.users.map(u => u.username).join(",");
+            return {
+                speaker: users,
+                text: e.text,
+                ts: e.ts
+            }
+        });
+        console.log('++++++++',this.callLog);
+    }
+
+
+    //==========================================HANDLERS===============================
+    //=================================================================================
+    handleCSVDownload() {
+        this.downloadCSVDisable = true;
+        let csv = "Speaker,Text,ts\n";
+        this.queriedData.forEach(e => {
+          csv += `"${e.users.map(u => u.username)}","${e.text}", "${e.ts}"\n`;
+        });
+        let downloadLink = document.createElement("a");
+        downloadLink.href = "data:text/csv;charset=utf-8," + encodeURIComponent(csv);
+        downloadLink.download = `transcription ${this.callerName} ${this.callDate}.csv`;
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+      }
 }
